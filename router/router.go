075f17b8f5dfedf5298cef1220b586c50391af9a
Marcos Silva <marcos.201314@hotmail.com>
2023-05-08 22:49:56 -0300

    fix: rename route to get all users

--- router/router.go
+++ router/router.go
@@ -9,7 +9,7 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/user", middleware.GetAllUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/users", middleware.GetAllUser).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/user/{id}", middleware.UpdateUser).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/deleteuser/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
