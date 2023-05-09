89d2024120a58dd26c1c5b009b6001e9b36dde62
Marcos Silva <marcos.201314@hotmail.com>
2023-05-08 21:46:36 -0300

    feat: add the CRUD methods and functions

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -3,11 +3,16 @@
 package middleware
 
 import (
+	"GOLANGCRUD/golang-CRUD/models"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -48,4 +53,273 @@ func createConnection() *sql.DB {
 
 	// Return the database connection
 	return db
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	// set the header to content type x-www-form-urlencoded
+	// Allow all origin to handle cors issue
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var user models.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+
+	insertID := insertUser(user)
+
+	res := response{
+		ID:      insertID,
+		Message: "User created!!",
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// get the userid from the request params, key is "id"
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert the string.  %v", err)
+	}
+
+	user, err := getUser(int64(id))
+
+	if err != nil {
+		log.Fatalf("Cannot find the user. %v", err)
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func GetAllUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// get all the users in the db
+	users, err := getAllUsers()
+
+	if err != nil {
+		log.Fatalf("Unable to get all users. %v", err)
+	}
+
+	// send all the users as response
+	json.NewEncoder(w).Encode(users)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Cannot find the User by ID. %v", err)
+	}
+
+	var user models.User
+
+	// decoding json request to user
+	err = json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the body. %v", err)
+	}
+
+	updatedRows := updateUser(int64(id), user)
+
+	msg := fmt.Sprintf("User updated! Total rows/record affected %v", updatedRows)
+
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Cannot convert the user ID. %v", err)
+	}
+
+	deletedRows := deleteUser(int64(id))
+
+	msg := fmt.Sprintf("User deleted! Total rows/record affected %v", deletedRows)
+
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func insertUser(user models.User) int64 {
+
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	sqlStatement := `INSERT INTO users (name, location, age) values $1, $2, $3 RETURNING userid`
+
+	var id int64
+
+	// execute the sql statement
+	// Scan function will save the insert id in the id
+	err := db.QueryRow(sqlStatement, user.Name, user.Location, user.Age).Scan(&id)
+
+	if err != nil {
+		log.Fatalf("Cannot run the querie. %v", err)
+	}
+
+	fmt.Printf("User inserted! %v", id)
+
+	return id
+}
+
+func getUser(id int64) (models.User, error) {
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	// create a user of models.User type
+	var user models.User
+
+	// create the select sql query
+	sqlStatement := `SELECT * FROM users WHERE userid=$1`
+
+	row := db.QueryRow(sqlStatement, id)
+
+	err := row.Scan(&user.ID, &user.Name, &user.Age, &user.Location)
+
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows returned!")
+		return user, nil
+	case nil:
+		return user, nil
+	default:
+		log.Fatalf("Unable to scan the row. %v", err)
+	}
+
+	return user, err
+}
+
+func getAllUsers() ([]models.User, error) {
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	var users []models.User
+
+	sqlStatement := `SELECT * FROM users`
+
+	rows, err := db.Query(sqlStatement)
+
+	if err != nil {
+		log.Fatalf("Cannot run the query. %v", err)
+	}
+	// close the statement
+	defer rows.Close()
+
+	for rows.Next() {
+		var user models.User
+
+		err := rows.Scan(&user.ID, &user.Name, &user.Age, &user.Location)
+
+		if err != nil {
+			log.Fatalf("Cannot cannot scan the row. %v", err)
+		}
+
+		users = append(users, user)
+	}
+
+	return users, err
+}
+func updateUser(id int64, user models.User) int64 {
+
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	sqlStatement := `UPDATE users SET name=$2, location=$3, age=$4 WHERE userid=$1`
+
+	res, err := db.Exec(sqlStatement, id, user.Name, user.Location, user.Age)
+
+	if err != nil {
+		log.Fatalf("Cannot execute the query. %v", err)
+	}
+
+	// check how many rows affected
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+
+	fmt.Printf("Total rows/record affected %v", rowsAffected)
+
+	return rowsAffected
+}
+
+func deleteUser(id int64) int64 {
+
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	sqlStatement := `DELETE FROM users WHERE userid=$1`
+
+	res, err := db.Exec(sqlStatement, id)
+
+	if err != nil {
+		log.Fatalf("Cannot execute the query. %v", err)
+	}
+	// check how many rows affected
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+
+	fmt.Printf("Total rows/record affected %v", rowsAffected)
+
+	return rowsAffected
 }
